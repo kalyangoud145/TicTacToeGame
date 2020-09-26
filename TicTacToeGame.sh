d1a37a803b13573eccada8fac2fd72e2b58165b5
kalyangoud145 <kalyangoud145@gmail.com>
2020-09-26 10:16:16 +0530

    UC2 choosing user input X or O

--- TicTacToeGame.sh
+++ TicTacToeGame.sh
@@ -8,6 +8,25 @@ function createBoard()
       board[$index]=$NULL
    done
 }
+function assignLetterXOrO(){
+   local assign=1
+   random=$((RANDOM%2))
+   if [ $assign -eq $random ]
+        then
+            playerSymbol=X
+            computerSymbol=O
+            echo "player:"
+            echo  $playerSymbol
+            echo "computer:"
+            echo  $computerSymbol
+        else
+            playerSymbol=O
+            computerSymbol=X
+            echo "player:" $playerSymbol
+            echo "computer:" $computerSymbol
+        fi
+}
+
 function TicTacToeGame(){
    createBoard
    assignLetterXOrO
