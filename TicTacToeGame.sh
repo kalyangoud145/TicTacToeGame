e4b2fd40a1e1724213781a395c7d7b67cf6a306d
kalyangoud145 <kalyangoud145@gmail.com>
2020-09-26 10:09:58 +0530

    UC1 commited

---
+++ TicTacToeGame.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+NULL=" "
+declare -a board
+function createBoard()
+{
+   for (( index=1; index<=9; index++ ))
+   do
+      board[$index]=$NULL
+   done
+}
+function TicTacToeGame(){
+   createBoard
+   assignLetterXOrO
+}
+TicTacToeGame
