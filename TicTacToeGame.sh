7bca95d13b84eced17d6314d9dc9b8116f421930
kalyangoud145 <kalyangoud145@gmail.com>
2020-09-26 10:43:10 +0530

    Refactored

--- TicTacToeGame.sh
+++ TicTacToeGame.sh
@@ -1,34 +1,12 @@
 #!/bin/bash -x
 NULL=" "
 declare -a board
-function createBoard()
-{
-   for (( index=1; index<=9; index++ ))
-   do
-      board[$index]=$NULL
-   done
-}
 function assignLetterXOrO(){
-   local assign=1
-   random=$((RANDOM%2))
-   if [ $assign -eq $random ]
-        then
-            playerSymbol=X
-            computerSymbol=O
-            echo "player:"
-            echo  $playerSymbol
-            echo "computer:"
-            echo  $computerSymbol
-        else
-            playerSymbol=O
-            computerSymbol=X
-            echo "player:" $playerSymbol
-            echo "computer:" $computerSymbol
-        fi
+	read "Enter player choice X or O" playerSymbol
+	read "Enter computer choice X or O" computerSymbol
 }
 
 function TicTacToeGame(){
-   createBoard
    assignLetterXOrO
 }
 TicTacToeGame
